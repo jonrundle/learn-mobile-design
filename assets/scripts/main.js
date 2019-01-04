a09194a857c561ce307fd619c6ed45d01e3d5878
Jon Rundle <jonrundle@gmail.com>
2019-01-04 09:57:14 -0500

    Show/hide animations for form

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,14 +11,64 @@ function setCurrentNav() {
 }
 
 $(".js-nav-link").click(function(e) {
-    e.preventDefault();
-    var aid = $(this).attr("href");
-    $('html,body').animate({scrollTop: $(aid).offset().top},'fast');
-    setCurrentNav();
+  e.preventDefault();
+  var aid = $(this).attr("href");
+  $('html,body').animate({scrollTop: $(aid).offset().top},'fast');
+  setCurrentNav();
 });
 
 $(document).scroll(function() {
   setCurrentNav();
+});
+
+function showForm() {
+  $('.js-form-wrapper').addClass('flex');
+  $('.js-form-wrapper').removeClass('flex-none dn');
+  var fadeBG = anime({
+    targets: '.js-form-bg',
+    opacity: [0, 1],
+    duration: 300,
+    easing: 'linear'
+  });
+  var showForm = anime({
+    targets: '.js-form-box',
+    opacity: [0, 1],
+    translateY: [80, 0],
+    scale: [0.9, 1],
+    duration: 400,
+    easing: [0.175, 0.885, 0.32, 1.6]
+  });
+}
+
+function hideForm() {
+  var showForm = anime({
+    targets: '.js-form-box',
+    opacity: [1, 0],
+    translateY: [0, 80],
+    scale: [1, 0.9],
+    duration: 400,
+    easing: [0.175, 0.885, 0.32, 1.6]
+  });
+  var fadeBG = anime({
+    targets: '.js-form-bg',
+    opacity: [1, 0],
+    duration: 300,
+    easing: 'linear'
+  });
+  setTimeout(function(){
+    $('.js-form-wrapper').addClass('flex-none dn');
+    $('.js-form-wrapper').removeClass('flex');
+  }, 200);
+}
+
+$(".js-submit-link").click(function(e) {
+  e.preventDefault();
+  showForm();
+});
+
+$(".js-close-form").click(function(e) {
+  e.preventDefault();
+  hideForm();
 });
 
 $(function(){
@@ -33,6 +83,8 @@ $(function(){
             success: function(response){
                 if(response.status == "success"){
                     alert("We received your submission, thank you!");
+                    hideForm();
+                    $('.js-submit-form')[0].reset();
                 }else{
                     alert("An error occured: " + response.message);
                 }
