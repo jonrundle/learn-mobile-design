87d22bf56b48d108cdbd100237bd6bc3855b6d06
Jon Rundle <jonrundle@gmail.com>
2019-01-04 09:25:18 -0500

    Hookup form to formcarry

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -19,4 +19,24 @@ $(".js-nav-link").click(function(e) {
 
 $(document).scroll(function() {
   setCurrentNav();
-})
+});
+
+$(function(){
+    $(".js-submit-form").submit(function(e){
+        e.preventDefault();
+        var href = $(this).attr("action");
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: href,
+            data: $(this).serialize(),
+            success: function(response){
+                if(response.status == "success"){
+                    alert("We received your submission, thank you!");
+                }else{
+                    alert("An error occured: " + response.message);
+                }
+            }
+        });
+    });
+});
