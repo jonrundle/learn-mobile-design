1dd4c10900338f31390cc25749137768e3050f29
Jon Rundle <jon@envoy.com>
2021-06-08 21:03:39 -0400

    Starting new ui

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  fontFamily: {
+    sans: ['-apple-system', 'system-ui', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Ubuntu', 'sans-serif'],
+  },
+  variants: {},
+  plugins: [],
+}
