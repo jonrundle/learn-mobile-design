25c75bbcdd9dea8677248e57375c9f2461d9b597
Jon Rundle <jon@envoy.com>
2021-06-09 20:54:18 -0400

    Base styling for 80% of the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white96: 'rgba(255,255,255,0.96)',
+        darkGrey: '#232323',
+        blue: '#4377E3',
+        blueLight: '#1BABF2',
+        red: '#FA564A',
+      },
+      borderRadius: {
+        '4xl': '2.5rem',
+        '5xl': '6rem',
+      },
+      boxShadow: {
+        '3xl': '0px 8px 40px rgba(0, 0, 0, 0.16)',
+      },
+    },
   },
   fontFamily: {
     sans: ['-apple-system', 'system-ui', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Ubuntu', 'sans-serif'],
