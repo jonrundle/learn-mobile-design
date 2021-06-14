ed65fed631ff1a558382f890a34a1e848bacc251
Jon Rundle <jon@envoy.com>
2021-06-13 21:36:41 -0400

    More floating nav logic and styling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,13 +3,12 @@ function setCurrentNav() {
   $(".js-category").each(function() {
     var offset = ($(this).position().top) - 140;
     var title = $(this).data("title");
-    var id = $(this).attr("id");
     var prev = $(this).prev().attr("id");
     var next = $(this).next().attr("id");
     if(scrollVal >= offset) {
       $('.js-nav-title').html(title);
-      console.log(prev);
-      console.log(next);
+      $('.js-nav-prev').attr('href', '#' + prev);
+      $('.js-nav-next').attr('href', '#' + next);
     }
   });
 }
@@ -23,4 +22,12 @@ $(".js-nav-link").click(function(e) {
 
 $(document).scroll(function() {
   setCurrentNav();
+  var docScroll = $(document).scrollTop();
+  if(docScroll > 300) {
+    $('.js-footer').removeClass('opacity-0 -bottom-10');
+    $('.js-footer').addClass('opacity-100 bottom-4');
+  } else {
+    $('.js-footer').addClass('opacity-0 -bottom-10');
+    $('.js-footer').removeClass('opacity-100 bottom-4');
+  }
 });
