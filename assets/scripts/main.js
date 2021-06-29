4bea11d2c3ae867383f924b41bc51d0e44a93161
Jon Rundle <jon@envoy.com>
2021-06-28 20:06:33 -0400

    Tweaking bottom values for nav

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -23,12 +23,12 @@ var navOpen = false;
 $(".js-footer").click(function(e) {
   if(navOpen) {
     $('.floating-nav').addClass('max-h-0 opacity-0 bottom-10');
-    $('.floating-nav').removeClass('opacity-100 bottom-24 max-h-full');
+    $('.floating-nav').removeClass('opacity-100 bottom-28 max-h-full');
     $('.js-nav-arrow ').removeClass('rotate-180');
     navOpen = false;
   } else {
     $('.floating-nav').removeClass('max-h-0 opacity-0 bottom-10');
-    $('.floating-nav').addClass('opacity-100 bottom-24 max-h-full');
+    $('.floating-nav').addClass('opacity-100 bottom-28 max-h-full');
     $('.js-nav-arrow ').addClass('rotate-180');
     navOpen = true;
   }
@@ -37,14 +37,14 @@ $(".js-footer").click(function(e) {
 $(document).scroll(function() {
   setCurrentNav();
   var docScroll = $(document).scrollTop();
-  if(docScroll > 100) {
+  if(docScroll > 80) {
     $('.js-footer').removeClass('opacity-0 -bottom-10');
-    $('.js-footer').addClass('opacity-100 bottom-4');
+    $('.js-footer').addClass('opacity-100 bottom-8');
   } else {
     $('.js-footer').addClass('opacity-0 -bottom-10');
-    $('.js-footer').removeClass('opacity-100 bottom-4');
+    $('.js-footer').removeClass('opacity-100 bottom-8');
     $('.floating-nav').addClass('max-h-0 opacity-0 bottom-10');
-    $('.floating-nav').removeClass('opacity-100 bottom-24 max-h-full');
+    $('.floating-nav').removeClass('opacity-100 bottom-28 max-h-full');
     $('.js-nav-arrow ').removeClass('rotate-180');
     navOpen = false;
   }
